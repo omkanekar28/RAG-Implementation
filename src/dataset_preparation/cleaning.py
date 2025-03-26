0f1f8b46702e73650957235c96f3196dd4067976
omkanekar28 <omkanekar28@gmail.com>
2025-03-26 10:07:36 +0000

    Finished dataset cleaning

--- src/dataset_preparation/cleaning.py
+++ src/dataset_preparation/cleaning.py
@@ -3,30 +3,54 @@ import pandas as pd
 
 class DatasetCleaner:
     """
+    A class to clean the dataset for the recommendation system.
     """
     
-    def __init__(self, input_dataset_path: str) -> None:
+    def __init__(self, input_dataset_path: str, output_store_path: str) -> None:
         """
+        Initializes the DatasetCleaner object.
         """
         self.input_df = pd.read_csv(input_dataset_path)
-        self.output_df = self.input_df.copy()
+        print("Dataset loaded")
+        self.cleaned_df = self.input_df.copy()
         self.columns_to_remove = ["homepage", "id", "original_language", "original_title", "status", "vote_count", "crew"]
+        self.output_store_path = output_store_path
 
     def remove_unreleased(self) -> None:
         """
+        Removes all the movies that are not released.
         """
-        pass
+        self.cleaned_df = self.cleaned_df[self.cleaned_df["status"] == "Released"]
+        print("Unreleased movies removed")
         
     def remove_unimportant_columns(self) -> None:
         """
+        Removes all the columns that are not important for the recommendation system.
         """
-        pass
+        self.cleaned_df.drop(self.columns_to_remove, axis=1, inplace=True)
+        print("Unimportant columns removed")
 
     def remove_duplicate_rows(self) -> None:
         """
+        Removes all the duplicate rows from the dataset.
         """
-        pass
+        self.cleaned_df.drop_duplicates(inplace=True)
+        print("Duplicate rows removed")
+
+    def start_process(self) -> None:
+        print("Starting dataset cleaning process")
+        print(f"Dataset shape before cleaning: {self.cleaned_df.shape}")
+        self.remove_unreleased()
+        self.remove_unimportant_columns()
+        self.remove_duplicate_rows()
+        self.cleaned_df.to_csv(self.output_store_path, index=False)
+        print("Dataset cleaning completed")
+        print(f"Dataset shape after cleaning: {self.cleaned_df.shape}")
 
 
 if __name__ == "__main__":
-    cleaner = DatasetCleaner(input_dataset_path="/home/omkanekar28/code/RAG-Implementation/data/movie_dataset.csv")
+    cleaner = DatasetCleaner(
+        input_dataset_path="/home/omkanekar28/code/RAG-Implementation/data/movie_dataset.csv",
+        output_store_path="cleaned_movie_dataset.csv"
+    )
+    cleaner.start_process()
